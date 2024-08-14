5870e7f4ff2122a3c60541882787ad5539b05023
adriaanvw <avw@newspacesystems.com>
2024-08-14 08:04:52 +0200

    try simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,51 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:bluetooth_gate_control/bluetooth_service.dart';
+// import 'package:bluetooth_gate_control/ui/home_page.dart';
+// import 'package:bluetooth_gate_control/ui/theme.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (context) => BluetoothService(),
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Bluetooth Gate Control',
+//       theme: appTheme,
+//       home: HomePage(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:bluetooth_gate_control/bluetooth_service.dart';
-import 'package:bluetooth_gate_control/ui/home_page.dart';
-import 'package:bluetooth_gate_control/ui/theme.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => BluetoothService(),
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bluetooth Gate Control',
-      theme: appTheme,
-      home: HomePage(),
+      title: 'Test App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Test App'),
+        ),
+        body: const Center(
+          child: Text('Hello, iPhone!', style: TextStyle(fontSize: 24)),
+        ),
+      ),
     );
   }
 }
