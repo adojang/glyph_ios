bc21329d00ce7a1888d7cb5fc25112efcf9e91e0
Adriaan van Wijk <48274045+adojang@users.noreply.github.com>
2024-08-16 09:08:48 +0200

    Try out Baiscs
    Basics

--- lib/ui/theme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-final ThemeData appTheme = ThemeData(
-  primarySwatch: Colors.blue,
-  visualDensity: VisualDensity.adaptivePlatformDensity,
-  brightness: Brightness.light,
-);
